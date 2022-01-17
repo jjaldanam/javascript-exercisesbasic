6827807e9b16ded0f6867a3432690c4347818aa3
jjaldanam <jjaldana@gmail.com>
2022-01-17 12:07:38 -0500

    Adicionar ejercicio 16

---
+++ ej16_formulario1.js
@@ -0,0 +1,8 @@
+/*
+16. Ejercicio con DOM y eventos
+
+	1. Formulario campos: Nombre, Apellidos y edad  -OK
+	2. Boton de enviar el formulario: eventos submit -OK
+	3. Mostrar datos por pantalla
+	4. Validación del formulario para que no metan datos vacíos
+ */
